28e8d359b2a7848b06cf81e9f088426bdf943043
Christian Toney <37517470+Sudobeast@users.noreply.github.com>
2022-05-27 19:18:14 -0400

    change to PUT

--- classes/Client.js
+++ classes/Client.js
@@ -533,7 +533,8 @@ export default class Client {
 
     // Send a request to the server to update the blog post.
     await this.requestREST(`contents/blog/${username}/${slug}`, {
-      body: formData
+      body: formData,
+      method: "PUT"
     });
 
   }
