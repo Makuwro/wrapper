4ed593c4380a6fed756b14962e8c6be6d872e188
Christian Toney <christian@makuwro.com>
2022-07-20 09:52:24 -0400

    change extension to content

--- classes/Comment.js
+++ classes/Comment.js
@@ -1,4 +1,4 @@
-import Account from "./Account.js";
+import Content from "./Content.js";
 
 /**
  * Represents a comment.
@@ -6,7 +6,7 @@ import Account from "./Account.js";
  * @prop {User|Team} owner The owner of the comment.
  * @prop {String} content The content of the comment.
  */
-export default class Comment extends Account {
+export default class Comment extends Content {
 
   constructor(data, client) {
 
